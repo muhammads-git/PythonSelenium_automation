3ccf1ab5d34a7086296fe1045bf28f10d6c14497
muhammads-git <roadprogrammar@gmail.com>
2025-07-07 20:11:46 +0500

    FINAL VERSION

--- Testranking_cric.py
+++ Testranking_cric.py
@@ -11,9 +11,10 @@ def setup_function():
     global driver
     option = webdriver.FirefoxOptions()
     driver = webdriver.Remote(
-        command_executor=hub_url,
-        options = option
+    command_executor=hub_url,
+    options = option
     )
+
     driver.implicitly_wait(5)
 
 def teardown_function():
@@ -25,12 +26,20 @@ def test_cricbuzz_automation():
 
     driver.get('https://www.cricbuzz.com/cricket-stats/icc-rankings/men/batting')
     time.sleep(3)   # optional can be replaced with explicit wait
-    
+
+    # title of the page
+    print("Title of the page is: ", driver.title)
+
     cols_table = driver.find_elements(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[1]/div')
     rows_table = driver.find_elements(By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[1]/div[2]/div')
+    time.sleep(2)
 
     print('Total Columns are: ',len(cols_table))
     print('Total Rows are: ',len(rows_table))
+    
+    # lets take the screenshot and save it as png.
+    driver.get_screenshot_as_file('Cricbuzz.png')
+    print("Screenshot has been saved.")
 
     print(":::::::::::::RANKING::::::::::::::::")
 
@@ -43,5 +52,5 @@ def test_cricbuzz_automation():
     print("\n:::::::::::::END::::::::::::::::")
         
     # lets assert it
-    assert len(cols_table) == 6, "Column count mismatch"
+    assert len(cols_table) == 16, "Column count mismatch"
     assert len(rows_table) > 0, "No rows found in the table"
